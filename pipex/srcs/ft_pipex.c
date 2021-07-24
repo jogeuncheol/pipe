670b835533a9ac36a4edc1dd84ad7712a5206146
jogeuncheol <blueqwerty@naver.com>
2021-07-24 22:23:08 +0900

    fix ft_parent waitpid

--- pipex/srcs/ft_pipex.c
+++ pipex/srcs/ft_pipex.c
@@ -77,7 +77,7 @@ void	ft_child(t_pipe pip, char ***cmd_arr, char **envp)
 	}
 }
 
-void	ft_parent(t_pipe *pip, pid_t pid, int *status)
+void	ft_parent(t_pipe *pip, pid_t pid)
 {
 	if (pip->backup_fd == -1 && pip->cmd_count > 2)
 	{
@@ -87,7 +87,7 @@ void	ft_parent(t_pipe *pip, pid_t pid, int *status)
 	}
 	else
 		close(pip->fd[1]);
-	waitpid(pid, status, WNOHANG);
+	waitpid(pid, NULL, WNOHANG);
 	if (pip->backup_fd == -1 && pip->cmd_count > 2)
 		pip->backup_fd = pip->fd2[0];
 	else
@@ -98,7 +98,6 @@ void	ft_pipex(char ***cmd_arr, char **envp, int file1_fd, int file2_fd)
 {
 	t_pipe	pip;
 	pid_t	pid;
-	int		status;
 
 	ft_init_pipe(&pip, cmd_arr, file1_fd, file2_fd);
 	while (cmd_arr[pip.cmd_idx] != NULL)
@@ -111,22 +110,7 @@ void	ft_pipex(char ***cmd_arr, char **envp, int file1_fd, int file2_fd)
 		if (pid == 0)
 			ft_child(pip, cmd_arr, envp);
 		else if (pid > 0)
-		{
-			ft_parent(&pip, pid, &status);
-/*			if (pip.backup_fd == -1 && pip.cmd_count > 2)
-			{
-				close(pip.fd[0]);
-				close(pip.fd2[1]);
-				close(pip.fd[1]);
-			}
-			else
-				close(pip.fd[1]);
-			waitpid(pid, &status, WNOHANG);
-			if (pip.backup_fd == -1 && pip.cmd_count > 2)
-				pip.backup_fd = pip.fd2[0];
-			else
-				pip.backup_fd = pip.fd[0];*/
-		}
+			ft_parent(&pip, pid);
 		else
 			ft_error(NULL, NULL);
 		pip.cmd_idx++;
